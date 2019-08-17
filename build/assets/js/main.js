7ba82c6dbe667dd47c3e2e5a2572c91068733fb1
dVitold <tigronixx@gmail.com>
2019-08-17 18:52:15 +0300

    build

---
+++ build/assets/js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var qs = function qs(sel) {
+	return document.querySelector(sel);
+};
+var qsa = function qsa(sel) {
+	return document.querySelectorAll(sel);
+};
+var APP = {
+	name: 'iBrush HTML Starter'
+};
+(function () {})();
+//# sourceMappingURL=main.js.map
